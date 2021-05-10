af5d637253f34055da6febab8b442b37c49ffb14
Sergii Bogush <sergbogush@gmail.com>
2021-05-10 07:57:10 +0300

    delete consol.logs

--- src/index.js
+++ src/index.js
@@ -13,12 +13,9 @@ if (localStorage.getItem('theme') === 'light') {
 
 bodyRef.classList.add('light-theme');
 
-console.log(themeSwitchContainerRef);
-
 themeSwitchContainerRef.addEventListener('change', onSwitchThemeChanged);
 
 function onSwitchThemeChanged() {
-  console.log('Changed', themeSwitchContainerRef.checked);
   if (localStorage.theme === 'light') {
     bodyRef.classList.replace('light-theme', 'dark-theme');
     localStorage.setItem('theme', 'dark');
@@ -29,11 +26,8 @@ function onSwitchThemeChanged() {
 }
 
 import menuJs from './menu.json';
-
 import menuTemplate from './templates/menu.hbs';
 
 let markup = '';
-
 menuJs.forEach(item => (markup += menuTemplate(item)));
-
 menuContainerRef.insertAdjacentHTML('afterbegin', markup);
