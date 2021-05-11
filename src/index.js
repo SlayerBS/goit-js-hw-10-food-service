97a368f1822aff75a33040ea2ee2f180e75bf528
Sergii Bogush <sergbogush@gmail.com>
2021-05-11 06:53:32 +0300

    Changed index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
 const bodyRef = document.querySelector('body');
 const menuContainerRef = document.querySelector('.js-menu');
 const themeSwitchContainerRef = document.querySelector('#theme-switch-toggle');
@@ -6,12 +12,12 @@ themeSwitchContainerRef.addEventListener('change', onSwitchThemeChanged);
 
 //Проверка темы при первоначальной загрузке страницы.
 //Если сТраница раньше на загружалась, то theme: null
-if (localStorage.getItem('theme') === 'dark-theme') {
-  bodyRef.classList.add('dark-theme');
+if (localStorage.getItem('theme') === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
   themeSwitchContainerRef.checked = 'true';
 } else {
-  localStorage.setItem('theme', 'light-theme');
-  bodyRef.classList.add('light-theme');
+  localStorage.setItem('theme', Theme.LIGHT);
+  bodyRef.classList.add(Theme.LIGHT);
 }
 
 //импорт данных
@@ -23,13 +29,12 @@ const markup = createMarkup(menuJs);
 menuContainerRef.insertAdjacentHTML('afterbegin', markup);
 
 function onSwitchThemeChanged() {
-  //функция изменения фона при событии на инпуте
-  if (localStorage.theme === 'light-theme') {
-    bodyRef.classList.replace('light-theme', 'dark-theme');
-    localStorage.setItem('theme', 'dark-theme');
+  if (localStorage.theme === Theme.LIGHT) {
+    bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
   } else {
-    bodyRef.classList.replace('dark-theme', 'light-theme');
-    localStorage.setItem('theme', 'light-theme');
+    bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
   }
 }
 
